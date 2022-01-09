d304e6fd55ad3d0c05b155b3c21a29e59a71068e
DESKTOP-071RMTM\Administrator <930029203@qq.com>
2022-01-09 19:22:26 +0800

    修改配置

--- config/aop.php
+++ config/aop.php
@@ -1,14 +1,18 @@
 <?php
 //定义切入方法区分大小写
+use app\aop\TestAspect;
+use app\social\controller\Like;
+use  app\social\service\LikeService;
+
 return [
-    \app\aop\TestAspect::class => [
-        app\social\controller\Like::class => [
-            'list',
+    TestAspect::class => [ //切面
+        Like::class => [ //切入类
+            'list', //切入点
             'set',
         ],
-//        app\social\service\LikeService::class => [
-//            'list',
-//        ],
+        LikeService::class => [
+            'list',
+        ],
     ],
 ];
 
